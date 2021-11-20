80858a7b8f2cc63b00ddf9592d625d270eecf061
vfsouza <vini.f.souza@hotmail.com>
2021-11-20 00:13:32 -0300

    Add funcionalidade de cadastro de modulos

--- src/scripts/cadastro.js
+++ src/scripts/cadastro.js
@@ -27,7 +27,7 @@ $("#form-add-curso").submit(function (event) {
                <h1 class="mb-4 lh-1 font-weight-bold h2">Cadastro de Módulos</h1>
             </div>
             <div class="col-2 p-0">
-               <input type="number" id="#inputMod" name="inputMod" class="form-control cad"/>
+               <input value="0" onchange="modulos()" type="number" id="inputMod" name="inputMod" class="form-control cad"/>
             </div>
             <div class="col-2 p-0 pl-3">
                <input type="text" id="#inputId" name="inputId" class="form-control cad" value="${data}" disabled/>
@@ -42,14 +42,15 @@ $("#form-add-curso").submit(function (event) {
 });
 
 function criaModulos(num) {
+	console.log("criaModulos: ", num);
+	$("#modulos").html("");
 	for (let i = 0; i < num; i++) {
-		$("#modulos").html("");
 		$("#modulos").append(`
       <div class="card cardMod mb-4" style="z-index: 1">
          <div class="card-body p-6">
             <div>
                <div class="col-6 p-0">
-                  <input id="inputMod${i + 1}" type="text" name="modulo${i + 1}" class="form-control cad" placeholder="Titulo do modulo ${i + 1}" required />
+                  <input id="inputModTitulo${i + 1}" type="text" name="modulo${i + 1}" class="form-control cad" placeholder="Titulo do modulo ${i + 1}" required />
                </div>
             </div>
             <div class="form-group">
@@ -60,6 +61,7 @@ function criaModulos(num) {
       `);
 	}
 }
+
 var res;
 function showCursos() {
 	$.get("http://localhost:6789/cursos", function (data) {
@@ -156,4 +158,108 @@ function removeCurso(id) {
 			console.log(data);
 		});
 	}
+}
+
+function submitModulo(num) {
+	$("#form-add-modulo").preventDefault();
+	let titulo = $("#modTitulo").val();
+	let tituloModulo = $("#inputTitulo").val();
+	let idCurso = $("#inputIdCurso").val();
+	let horas = $("#modHorasMod").val();
+	let topico = $("#inputTopicoMod").val();
+	$.ajax({
+		url: "http://localhost:6789/cadastrar/modulo",
+		type: "GET",
+		data: {
+			idCurso: idCurso,
+			titulo: tituloModulo,
+			horas: horas,
+			topico: topico,
+		},
+	}).complete(function (idModulo) {
+		num == 1
+			? () => {
+					let url = $("#modURL").val();
+					let descricao = $("#modDescricao").val();
+					$.ajax({
+						url: "http://localhost:6789/cadastrar/video",
+						type: "GET",
+						data: {
+							id: idModulo,
+							titulo: titulo,
+							url: url,
+							horas: horas,
+							descricao: descricao,
+						},
+					});
+			  }
+			: () => {
+					let questao = $("#modQuestao").val();
+					$.ajax({
+						url: "http://localhost:6789/cadastrar/desafio",
+						type: "GET",
+						data: {
+							id: idModulo,
+							titulo: titulo,
+							questao: questao,
+						},
+					});
+			  };
+	});
+}
+
+function createContent(num) {
+	$("#modContent").html("");
+	$("#modContent").append(`
+      <div class="mb-4 mt-5">
+         <h1 class="mb-4 lh-1 font-weight-bold h2">Cadastro de ${num == 1 ? "Vídeo" : "Desafio"}</h1>
+      </div>
+      <div class="form-group">
+         <input id="modTitulo" type="text" class="form-control cad" placeholder="Titulo do ${num == 1 ? "Vídeo" : "Desafio"}" />
+      </div>
+   `);
+	$("#submit-button").html("");
+	$("#submit-button").append(`
+      <div class="form-group d-flex">
+         <div class="col-6 pl-0">
+            <input id="submit" type="submit" value="Cadastrar" id="btnInsert" class="btn btn-primary btn-block"  />
+         </div>
+         <div class="col-6 pr-0">
+            <input id="voltar" type="button" value="Voltar" id="btnVoltar" class="btn btn-primary btn-block" onclick="resetSubmitButton()"/>
+         </div>
+      </div>
+   `);
+	num == 1
+		? $("#modContent").append(`
+      <div class="form-group">
+         <input id="inputHorasMod" type="number" name="horasMod" class="form-control cad" placeholder="Horas" required />
+      </div>
+      <div class="form-group">
+         <input id="modURL" type="text" class="form-control cad" placeholder="URL" />
+      </div>
+      <div class="form-group">
+         <input id="modDescricao" type="text" class="form-control cad" placeholder="Descrição" />
+      </div>
+   `)
+		: $("#modContent").append(`
+      <div class="form-group">
+         <input id="modQuestao" type="text" class="form-control cad" placeholder="Questão" />
+      </div>
+   `);
+}
+
+function resetSubmitButton() {
+	$("#submit-button").html("");
+	$("#modContent").html("");
+	$("#modContent").append(`
+      <div class="form-group d-flex createContent">
+         <div class="col-5 p-0">
+            <input type="button" class="btn btn-primary btn-block" value="Adicionar Vídeo" onclick="createContent(1)" />
+         </div>
+         <div class="col-2 p-0 d-flex justify-content-center font-weight-bold" style="font-size: 18px; align-items: center; display: flex">OU</div>
+         <div class="col-5 p-0">
+            <input type="button" class="btn btn-primary btn-block" value="Adicionar Desafio" onclick="createContent(2)" />
+         </div>
+      </div>
+   `);
 }
